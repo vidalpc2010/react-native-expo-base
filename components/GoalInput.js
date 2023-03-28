46482c7501469772047a6e4868fa233fb9f7a8bc
vidalpc2010 <vidalpc2010@hotmail.com>
2023-03-28 09:34:42 -0600

    centered styles commit

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -7,7 +7,7 @@ import {
   TextInput,
   Button,
   Modal,
-  Image
+  Image,
 } from "react-native";
 import { useState } from "react";
 
@@ -18,29 +18,33 @@ function GoalInput(props) {
     setEnteredGoalText(enteredText);
   }
 
-  //feature-test1
-
   function addGoalHandler() {
     props.onAddGoal(enteredGoalText);
     setEnteredGoalText("");
   }
 
   return (
-    <Modal visible={props.visible} animationType="fade">
-      <View style={styles.inputContainer}>
-        <Image style={styles.image} source={require('../assets/goal.png')}/>
-        <TextInput
-          style={styles.textInput}
-          placeholder="your course goal!"
-          onChangeText={goalInputHandler}
-          value={enteredGoalText}
-        />
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}>
-            <Button title="Cancel" onPress={props.onCancel} color="#f31282" />
-          </View>
-          <View style={styles.button}>
-            <Button title="Add Goal!" onPress={addGoalHandler} color="#b180f0" />
+    <Modal transparent={true} visible={props.visible} animationType="slide">
+      <View style={styles.centeredView}>
+        <View style={styles.inputContainer}>
+          <Image style={styles.image} source={require("../assets/goal.png")} />
+          <TextInput
+            style={styles.textInput}
+            placeholder="your course goal!"
+            onChangeText={goalInputHandler}
+            value={enteredGoalText}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button title="Cancel" onPress={props.onCancel} color="#f31282" />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title="Add Goal!"
+                onPress={addGoalHandler}
+                color="#b180f0"
+              />
+            </View>
           </View>
         </View>
       </View>
@@ -49,6 +53,12 @@ function GoalInput(props) {
 }
 
 const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
   inputContainer: {
     // flexDirection: "row",
     flexDirection: "column",
@@ -56,31 +66,33 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     // marginBottom: 24,
-    padding:16,
+    padding: 16,
     // borderBottomWidth: 1,
     // borderBottomColor: "#cccccc",
-    backgroundColor:"#311b6b",
-    flex: 1,
+    backgroundColor: "#311b6b",
+    // flex: 1,
+    elevation: 5,
+    shadowOpacity: 0.25,
   },
   buttonContainer: {
-    marginTop:16,
+    marginTop: 16,
     flexDirection: "row",
   },
   button: {
     width: "30%",
     marginHorizontal: 8,
   },
-  image:{
-    width:100,
-    height:100,
-    margin:20
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
   },
   textInput: {
     borderWidth: 1,
-    backgroundColor:'#e4d0ff',
+    backgroundColor: "#e4d0ff",
     borderColor: "#e4d0ff",
-    color:'#120438',
-    borderRadius:6,
+    color: "#120438",
+    borderRadius: 6,
     width: "100%",
     padding: 16,
     // marginRight: 8,
